c7e8acdfe0339351ad86c3bbb91da8b345905893
SchneiderNejc <nejc.sch@gmail.com>
2024-02-15 10:51:37 +0100

    import and update deploy script

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,23 +6,21 @@
 // global scope, and execute the script.
 const hre = require("hardhat");
 
+// Parameters setup
+var initSupply = 10 ** 6; // One million (10^18 = convert wei to ether)
+
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const gldToken = await hre.ethers.deployContract("GLDToken", [initSupply]);
+  await gldToken.waitForDeployment();
+  console.log(`gldToken deployed to ${gldToken.target}`);
 
-  const lockedAmount = hre.ethers.parseEther("0.001");
+  const gameNft = await hre.ethers.deployContract("GameNft");
+  await gameNft.waitForDeployment();
+  console.log(`gameNft deployed to ${gameNft.target}`);
 
-  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
-
-  console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
-  );
+  const destruct = await hre.ethers.deployContract("Destruct", [gldToken.target]);
+  await destruct.waitForDeployment();
+  console.log(`destruct deployed to ${destruct.target}`);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
