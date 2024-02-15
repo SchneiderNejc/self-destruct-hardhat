d0e25826edeec66fcf606de1fab8af077a37c91c
SchneiderNejc <nejc.sch@gmail.com>
2024-02-15 20:37:48 +0100

    add networks

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,5 +7,25 @@ const accounts = {
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.13",
-};
+  //defaultNetwork: "localhost",
+  networks: {
+    hardhat: {
+    },
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/" + process.env.INFURA_KEY,
+      //chainId: 11155111,  //chainId is optional
+      accounts: accounts //to deploy from second account, use [accounts[1]]
+    },
+    goerli: {
+      url: "https://goerli.infura.io/v3/" + process.env.INFURA_KEY,
+      accounts: accounts
+    },
+    bsctestnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545/",
+      accounts: accounts
+    }
+  },
+  solidity: {
+    version: "0.8.13",
+  }
+}
