3b2a014ddeb0e3468a478753dca6b771a0a58d46
SchneiderNejc <nejc.sch@gmail.com>
2024-02-16 22:39:47 +0100

    add owner check

--- contracts/Destruct.sol
+++ contracts/Destruct.sol
@@ -17,6 +17,7 @@ contract Destruct {
     /// @param _token ERC20 token address that will be refunded upon selfdestruct
     constructor(address _token) {
         owner = msg.sender;
+        require(_token != owner, "Cant be owner");
         require(_token != address(0), "Cant be zero");
         token = IERC20(_token);
     }
