b7ed50b390aa9fb7e679e233c4731a1516e10e27
SchneiderNejc <nejc.sch@gmail.com>
2024-02-16 22:36:09 +0100

    add deployContracts()

--- test/Destruct.js
+++ test/Destruct.js
@@ -7,6 +7,18 @@ const { expect } = require("chai");
 
 describe("Self Destruct", function () {
 
+    async function deployContracts() {
+        const [owner] = await ethers.getSigners(); // Access the first signer from the array
+
+        var initSupply = 10 ** 6;
+        const GLDToken = await ethers.getContractFactory("GLDToken");
+        const token = await GLDToken.deploy(initSupply);
+
+        const Destruct = await ethers.getContractFactory("Destruct");
+        const destruct = await Destruct.deploy(token.target);
+
+        return { owner, token, destruct, initSupply };
+    }
 
 
 });
