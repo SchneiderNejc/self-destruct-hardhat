2daf585334f53ef8ee7c43892a5aa22bee8b5fdb
SchneiderNejc <nejc.sch@gmail.com>
2024-02-17 16:03:53 +0100

    add @dev disclaimer

--- contracts/Destruct.sol
+++ contracts/Destruct.sol
@@ -9,6 +9,10 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
  *  @author Nejc Schneider
  *  @notice In addition to refunding ether, this contract also refunds 
  *          custom ERC20 tokens upon selfdestruct.
+ *  @dev There is currently no way to specify which erc20 tokens are
+ *       accepted, so contract will receive all.
+ *  @dev There is also no conventional way to keep track of different erc20 tokens sent
+ *       to contract, so only the one specified in the constructor will be refunded.
  */
 contract Destruct {
     address public immutable owner;
